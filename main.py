1b79e8113126795bc5ff76b323f34755e357cca4
Jofre Faraudo <37667469+JofreFaraudo@users.noreply.github.com>
2019-06-19 20:12:52 +0200

    Ja descodifica!!!!

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ peaks = []
 llindar = 1500
 midindex = math.floor(posl/2)
 differences = []
+llpos = 0
+llposl = 40
+binary = []
+binaryf = []
+
+def mitjana(l):
+	t = 0;
+	for i in l:
+		t += i
+	return t/len(l)
 
 with open("5.wav","rb") as f:
 	byte = f.read(44)
@@ -37,5 +47,26 @@ while pos < len(n)-midindex:
 i = 0
 while i < len(peaks)-1:
 	differences.append(abs(peaks[i]-peaks[i+1]))
-	print(peaks[i])
 	i += 1
+differences = differences[15:-5]
+
+while len(differences) - llpos != llposl:
+	a = differences[llpos:llpos+llposl]
+	binary.append(a[0] < mitjana(a))
+	#print(mitjana(a))
+	llpos += 1
+i = 0
+while i < llposl-1:
+	i += 1
+	binary.append(a[i] < mitjana(a))
+
+i = 0
+b = True
+while i < len(binary)-1:
+	if binary[i] and binary[i+1] and b:
+		b = False
+	else:
+		binaryf.append(binary[i])
+		b = True
+	i += 1
+print(binaryf)
